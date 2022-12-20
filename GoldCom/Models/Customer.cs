a5c6c87a1d31563c678a64454b05c9bee1152f98
Katy248 <petrovanton247@gmail.com>
2022-12-20 19:13:09 +0300

    Добавлены остальные свойства класса

--- GoldCom/Models/Customer.cs
+++ GoldCom/Models/Customer.cs
@@ -10,4 +10,17 @@ public class Customer : DbEntity
     public string CompanyName { get; set; }
     public string CompanyAddress { get; set; }
     public string CompanyPhone { get; set; }
+    public string Mail { get; set; }
+    public string OGRN { get; set; }
+    public string INN { get; set; }
+    public string OKPO { get; set; }
+    public string OKVED { get; set; }
+    public string RepresentativeFirstName { get; set; }
+    public string RepresentativeSurname { get; set; }
+    public string RepresentativeLastName { get; set; }
+    public string RepresentativePhone { get; set; }
+    public string RepresentativeEmail { get; set; }
+    public string CeoFirstName { get; set; }
+    public string CeoSurname { get; set; }
+    public string CeoLastName { get; set;}
 }
