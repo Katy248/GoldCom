eb261b70275bc95b3cd250e582e391ea55c9e196
Katy248 <petrovanton247@gmail.com>
2022-12-22 21:19:20 +0300

    Создано окно для создания заказов

--- GoldCom/Views/RequestCreationWindow.xaml.cs
+++ GoldCom/Views/RequestCreationWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,8 +10,8 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using GoldCom.ViewModel;
+using GoldCom.Models;
 
 namespace GoldCom.Views
 {
@@ -19,9 +20,21 @@ namespace GoldCom.Views
     /// </summary>
     public partial class RequestCreationWindow : Window
     {
-        public RequestCreationWindow()
+        public RequestCreationWindow(User employee)
         {
+            viewModel = new RequestViewModel(employee);
             InitializeComponent();
+            this.DataContext = viewModel;
+            customersBox.ItemsSource = viewModel.Customers;
+            typesBox.ItemsSource = viewModel.MaterialTypes;
+
+        }
+        private readonly RequestViewModel viewModel;
+
+        private void enterButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.UpdateData();
+            Close();
         }
     }
 }
